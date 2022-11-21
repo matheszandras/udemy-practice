601db831e3d76e4fda0333466a477606d5419c25
Andras Mathesz <andras_mathesz@epam.com>
2022-11-21 11:32:50 +0100

    Add selectors to CookieSettingObjects

--- src/test/java/practice/pageobject/CookieSettingsObjects.java
+++ src/test/java/practice/pageobject/CookieSettingsObjects.java
@@ -11,15 +11,18 @@ public class CookieSettingsObjects extends BasePageObjects {
         super(driver);
     }
 
-    @FindBy(id = "onetrust-pc-btn-handler")
+    @FindBy(className = "ot-sdk-container")
     public WebElement cookiePopup;
+    @FindBy(className = "cookie-setting-link")
+    public WebElement cookiePopupSettings;
     @FindBy(id = "ot-header-id-C0003")
     public WebElement functionalCookieTab;
     @FindBy(css = "#ot-desc-id-C0003 > div.ot-grp-hdr1 > div.ot-tgl > label > span.ot-switch-nob")
     public WebElement functionalCookieSlider;
     @FindBy(css = "#onetrust-pc-sdk > div.ot-pc-footer > div.ot-btn-container > button")
+    //save-preference-btn-handler onetrust-close-btn-handler
     public WebElement cookieSettingsConfirmationButton;
-    @FindBy()
+    @FindBy(className = "ud-btn udlite-btn ud-btn-large udlite-btn-large ud-btn-link udlite-btn-link ud-text-sm link white-link")
             //(css = "#udemy > div.main-content-wrapper > footer > div.footer-section.footer-section-main > div.links-and-locale > ul:nth-child(4) > li:nth-child(3) > a")
 
     public WebElement cookieSettingsFooter;
